b140b870aac2bf5a0b7ad38d3967abb77c3f1fe1
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-09-08 15:05:49 -0700

    fix namespace and update dependencies

--- extensions/Worker.Extensions.ServiceBus/src/ServiceBusStartup.cs
+++ extensions/Worker.Extensions.ServiceBus/src/ServiceBusStartup.cs
@@ -1,18 +1,14 @@
-// Copyright (c) Jacob Viau. All rights reserved.
-// Licensed under the MIT. See LICENSE file in the project root for full license information.
-
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Core;
 using Microsoft.Azure.Functions.Worker.Extensions.Rpc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Samples.Extensions.Rpc.Worker;
 
-[assembly: WorkerExtensionStartup(typeof(Startup))]
+[assembly: WorkerExtensionStartup(typeof(ServiceBusStartup))]
 
-namespace Samples.Extensions.Rpc.Worker;
+namespace Microsoft.Azure.Functions.Worker;
 
-public sealed class Startup : WorkerExtensionStartup
+public sealed class ServiceBusStartup : WorkerExtensionStartup
 {
     public override void Configure(IFunctionsWorkerApplicationBuilder applicationBuilder)
     {
